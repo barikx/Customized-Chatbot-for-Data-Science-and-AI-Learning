9fc93d00eeb1fd92d23c64407a9e142185110115
Suman Barik <124221384+barikx@users.noreply.github.com>
2025-09-14 15:53:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# main.py
+import os
 from chatbot.app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    print('Running on : http://127.0.0.1:5000/')
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))  # Render injects PORT
+    print(f"Running on : http://0.0.0.0:{port}/")
+    app.run(host="0.0.0.0", port=port, debug=False)  # debug=False in production
